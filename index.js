a884e53b459d7d5890b756268daaf3bd41968c5a
hanzalahwaheed <hanzalah.w@gmail.com>
2024-09-24 11:49:48 +0530

    added matrix and gaussian methods funcs

--- index.js
+++ index.js
@@ -19,8 +19,12 @@ fs.readFile("test_case.json", "utf8", (err, data) => {
 
   // Find secret 'c'
   const c = lagrangeInterpolation(points, testCase.keys.k);
+  const cc = gaussElimination(points, testCase.keys.k);
+  const ccc = matrixMethod(points, testCase.keys.k);
 
-  console.log("c =", c);
+  console.log("c (lagrange_method)=", c);
+  console.log("c (gauss_elim)", cc);
+  console.log("c (matrix_method)", ccc);
 });
 
 // Lagrange interpolation
@@ -40,4 +44,90 @@ function lagrangeInterpolation(points, k) {
   }
 
   return result;
+}
+
+// Gauss elimination
+function gaussElimination(points, k) {
+  const m = k - 1; // Degree of the polynomial
+  const A = [];
+  const b = [];
+
+  for (let i = 0; i < k; i++) {
+    const row = [];
+    for (let j = m; j >= 0; j--) {
+      row.push(Math.pow(points[i][0], j));
+    }
+    A.push(row);
+    b.push(points[i][1]);
+  }
+
+  // Augmented matrix
+  const augmentedMatrix = A.map((row, i) => [...row, b[i]]);
+
+  // Forward elimination
+  for (let i = 0; i < m; i++) {
+    // Pivot selection (partial pivoting - optional)
+    // ...
+
+    // Elimination
+    for (let j = i + 1; j < k; j++) {
+      const factor = augmentedMatrix[j][i] / augmentedMatrix[i][i];
+      for (let k = i; k <= m + 1; k++) {
+        augmentedMatrix[j][k] -= factor * augmentedMatrix[i][k];
+      }
+    }
+  }
+
+  // Back substitution
+  const x = new Array(m + 1).fill(0);
+  for (let i = m; i >= 0; i--) {
+    x[i] = augmentedMatrix[i][m + 1];
+    for (let j = i + 1; j <= m; j++) {
+      x[i] -= augmentedMatrix[i][j] * x[j];
+    }
+    x[i] /= augmentedMatrix[i][i];
+  }
+
+  return x[m]; // The constant term 'c'
+}
+
+// Matrix method (using a simple Gaussian elimination-based solver)
+function matrixMethod(points, k) {
+  const m = k - 1;
+  const A = [];
+  const b = [];
+
+  for (let i = 0; i < k; i++) {
+    const row = [];
+    for (let j = m; j >= 0; j--) {
+      row.push(Math.pow(points[i][0], j));
+    }
+    A.push(row);
+    b.push(points[i][1]);
+  }
+
+  // Augmented matrix
+  const augmentedMatrix = A.map((row, i) => [...row, b[i]]);
+
+  // Gaussian elimination (same as in gaussElimination function)
+  for (let i = 0; i < m; i++) {
+    for (let j = i + 1; j < k; j++) {
+      const factor = augmentedMatrix[j][i] / augmentedMatrix[i][i];
+      for (let k = i; k <= m + 1; k++) {
+        augmentedMatrix[j][k] -= factor * augmentedMatrix[i][k];
+      }
+    }
+  }
+
+  // Back substitution
+  const x = new Array(m + 1).fill(0);
+  for (let i = m; i >= 0; i--) {
+    x[i] = augmentedMatrix[i][m + 1];
+    for (let j = i + 1; j <= m; j++) {
+      x[i] -= augmentedMatrix[i][j] * x[j];
+    }
+    x[i] /= augmentedMatrix[i][i];
+  }
+
+  return x[m];
 }
